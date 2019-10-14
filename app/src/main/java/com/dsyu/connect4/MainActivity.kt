8908170d1cb6d728aa0bb2b30bfce81095adc845
Daniel Yu <daniel.sm.yu@gmail.com>
2019-10-14 16:15:43 -0400

    locked orientation

--- app/src/main/java/com/dsyu/connect4/MainActivity.kt
+++ app/src/main/java/com/dsyu/connect4/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayout
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -48,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+            updateBoard()
         }
 
         restartButton.setOnClickListener {
